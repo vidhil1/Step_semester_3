462a6d7771bdffc767fb663109d93f5dcf10aa0a
vidhil1 <vv7559@srmist.edu.in>
2026-09-13 15:01:09 +0530

    Add Week 6 Problem 3 WorkshopTicket

---
+++ src/main/java/oop/assignment/week6/problem3/WorkshopTicket.java
@@ -0,0 +1,11 @@
+public class WorkshopTicket extends EventTicket {
+
+    public WorkshopTicket(double basePrice) {
+        super(basePrice);
+    }
+
+    @Override
+    protected void applyLateFee(double amount) {
+        super.applyLateFee(amount * 2);
+    }
+}
