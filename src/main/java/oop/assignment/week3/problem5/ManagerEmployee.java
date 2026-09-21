162f0f95b330b6d52bcf986b79fc12145b017ea3
vidhil1 <vv7559@srmist.edu.in>
2026-09-22 00:07:19 +0530

    Add ManagerEmployee class with team bonus feature

---
+++ src/main/java/oop/assignment/week3/problem5/ManagerEmployee.java
@@ -0,0 +1,19 @@
+public class ManagerEmployee extends Employee {
+
+    private double teamBonus;
+
+    public ManagerEmployee(
+            int empId,
+            String empName,
+            double salary,
+            double teamBonus) {
+
+        super(empId, empName, salary);
+        this.teamBonus = teamBonus;
+    }
+
+    public double effectiveSalary() {
+
+        return getSalary() + teamBonus;
+    }
+}
