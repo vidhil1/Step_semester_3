9095c2c62776540979ed8af9011a9f84fe37371f
vidhil1 <vv7559@srmist.edu.in>
2026-09-22 00:00:06 +0530

    Create Employee.java

---
+++ src/main/java/oop/assignment/week3/problem2/Employee.java
@@ -0,0 +1,24 @@
+public class Employee {
+
+    private int empId;
+    private String empName;
+    private double salary;
+
+    public Employee(int empId, String empName, double salary) {
+        this.empId = empId;
+        this.empName = empName;
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+}
