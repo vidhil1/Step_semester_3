5073f9d3392388fbe1fd988686d8fe482850c38b
vidhil1 <vv7559@srmist.edu.in>
2026-09-21 22:18:18 +0530

    Create SecuritySensor.java

---
+++ src/main/java/oop/practice/week7/problem2/SecuritySensor.java
@@ -0,0 +1,12 @@
+public class SecuritySensor {
+
+    protected String zoneName;
+
+    public SecuritySensor(String zoneName) {
+        this.zoneName = zoneName;
+    }
+
+    public String getZoneName() {
+        return zoneName;
+    }
+}
