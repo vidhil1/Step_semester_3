c9ea4413a0d981ffa0ab64f45825475ddbb6d18b
vidhil1 <vv7559@srmist.edu.in>
2026-09-21 22:46:10 +0530

    Add DigitalPass class implementing Renewable interface

---
+++ src/main/java/oop/practice/week7/problem5/DigitalPass.java
@@ -0,0 +1,13 @@
+public class DigitalPass implements Renewable {
+
+    private String resourceName;
+
+    public DigitalPass(String resourceName) {
+        this.resourceName = resourceName;
+    }
+
+    @Override
+    public String renew() {
+        return resourceName + " renewed";
+    }
+}
