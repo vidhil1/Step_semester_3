ad811b2b01d10586f2a482193865011aa2c89424
vidhil1 <vv7559@srmist.edu.in>
2026-09-21 22:21:54 +0530

    Add abstract StaffMember class with salary management

---
+++ src/main/java/oop/practice/week7/problem3/StaffMember.java
@@ -0,0 +1,27 @@
+public abstract class StaffMember {
+
+    private double baseSalary;
+    protected double bonusRate;
+
+    public StaffMember(double baseSalary) {
+        this(baseSalary, 0.10);
+    }
+
+    public StaffMember(double baseSalary, double bonusRate) {
+        this.baseSalary = baseSalary;
+        this.bonusRate = bonusRate;
+    }
+
+    public abstract double calculateBonus();
+
+    public double getSalary() {
+        return baseSalary;
+    }
+
+    public void setSalary(double baseSalary) {
+
+        if (baseSalary >= 0) {
+            this.baseSalary = baseSalary;
+        }
+    }
+}
