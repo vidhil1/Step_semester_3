7d4beaa04e251b12ef292e473e9b5748d2d56b35
vidhil1 <vv7559@srmist.edu.in>
2026-09-21 22:19:46 +0530

    Add SmokeDetector class implementing Alertable interface

---
+++ src/main/java/oop/practice/week7/problem2/SmokeDetector.java
@@ -0,0 +1,13 @@
+public class SmokeDetector implements Alertable {
+
+    private String deviceId;
+
+    public SmokeDetector(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    @Override
+    public String sendAlert(String message) {
+        return "[" + deviceId + "] " + message;
+    }
+}
